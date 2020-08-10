27e1b6ec74c1cc5ed30ee1cf9ea1114002e30a87
Gabriel Anguiano <anguianog97@gmail.com>
2020-08-10 08:31:59 -0700

    added some dependencies and components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import { Route, Switch } from 'react-router-dom'
+import HomePage from './components/Home/Home';
 
 function App() {
   return (
     <div className="App">
-        <h1>Website in maintence...</h1>
+      <Switch>
+        <Route path='/' component={HomePage}/>
+        </Switch>
     </div>
   );
 }
---
+++ src/components/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HomePage = () => {
+
+    return (
+        <>
+         <h1>Home Page under Construction</h1>
+        </>
+    )
+}
+
+export default HomePage
