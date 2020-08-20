cc498c42ccd6e24b7be11ac20102085bcba231be
Gabriel Anguiano <anguianog97@gmail.com>
2020-08-20 15:35:13 -0700

    found a carousel library to implement a neat image slider for homepage

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,23 +1,46 @@
 import React from 'react'
 import { items } from '../ShoppingCartPage/ShoppingCartDummyData'
-
+import Slider from 'react-slick'
 import './Home.scss'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const HomePage = ({cart, setCart}) => {
 
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    }
 
 const addItem = () => {
     setCart([...cart, 1])
 }
 
     return (
-        <>
-         <div className="parent-container-slider">
-             <div className="item-one"></div>
-             <div className="item-two"></div>            
-             <div className="item-three"></div>            
-         </div>
-        </>
+        <div>
+        <Slider {...settings}>
+          <div className="item-one">
+          </div>
+          <div>
+            <h3>2</h3>
+          </div>
+          <div>
+            <h3>3</h3>
+          </div>
+          <div>
+            <h3>4</h3>
+          </div>
+          <div>
+            <h3>5</h3>
+          </div>
+          <div>
+            <h3>6</h3>
+          </div>
+        </Slider>
+        </div>
     )
 }
 
