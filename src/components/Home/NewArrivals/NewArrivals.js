1e09759415cf937218be614b8e2aa8102d356210
Gabriel Anguiano <anguianog97@gmail.com>
2020-08-25 15:03:34 -0700

    displaying items by local data first

--- src/components/Home/NewArrivals/NewArrivals.js
+++ src/components/Home/NewArrivals/NewArrivals.js
@@ -1,12 +1,20 @@
 import React, { useState, useEffect } from 'react';
-
+import { ArrivalProducts } from './NewArrivalsProducts'
+import './NewArrivals.scss'
 const NewArrivals = () => {
     const [items, setItems] = useState([])
     
 
+
+
+   useEffect(() => {
+        
+   }, [])
+
     return (
         <div>
         <h2 className="title">New Arrivals</h2>
+        <ArrivalProducts/> 
         </div>
     )
 }
