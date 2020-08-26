d17095f121de19925ffdf0a2177d25136794baf5
Gabriel Anguiano <anguianog97@gmail.com>
2020-08-26 14:04:38 -0700

    fixed some styling issues, which were some blockers

--- src/components/Home/NewArrivals/NewArrivalsProducts.js
+++ src/components/Home/NewArrivals/NewArrivalsProducts.js
@@ -3,6 +3,7 @@ import './NewArrivals.scss'
 export const ArrivalProducts = ({name, img, desc, price}) => {
 
     return (
+        <div className="product-arrival-section">
         <div className="arrival-product-container">
           <div className="arrival-product">
               <img className="product-image" src="https://cdn11.bigcommerce.com/s-eb373/images/stencil/1200x1200/products/7325/29022/muay-thai-boxing-gloves-yokkao-underground-3_1024x1024__12040.1591413418.jpg?c=2"/>
@@ -12,6 +13,19 @@ export const ArrivalProducts = ({name, img, desc, price}) => {
               <img className="product-image" src="https://cdn11.bigcommerce.com/s-eb373/images/stencil/1200x1200/products/7278/28807/c83eab03c1ca905732d002906896452a2fb73a35_bg_giant_3.0_white_gold_1500_02_1__3__47621.1581613909.jpg?c=2"/>
              <h3>Item Two</h3>
           </div>
+          <div className="arrival-product">
+              <img className="product-image" src="https://cdn11.bigcommerce.com/s-eb373/images/stencil/1200x1200/products/7278/28807/c83eab03c1ca905732d002906896452a2fb73a35_bg_giant_3.0_white_gold_1500_02_1__3__47621.1581613909.jpg?c=2"/>
+             <h3>Item Two</h3>
+          </div>
+          <div className="arrival-product">
+              <img className="product-image" src="https://cdn11.bigcommerce.com/s-eb373/images/stencil/1200x1200/products/7278/28807/c83eab03c1ca905732d002906896452a2fb73a35_bg_giant_3.0_white_gold_1500_02_1__3__47621.1581613909.jpg?c=2"/>
+             <h3>Item Two</h3>
+          </div>
+          <div className="arrival-product">
+              <img className="product-image" src="https://cdn11.bigcommerce.com/s-eb373/images/stencil/1200x1200/products/7278/28807/c83eab03c1ca905732d002906896452a2fb73a35_bg_giant_3.0_white_gold_1500_02_1__3__47621.1581613909.jpg?c=2"/>
+             <h3>Item Two</h3>
+          </div>
+        </div>
         </div>
     )
 }
