09ab242d004a52c826a44532e17e03833afdcc4e
Gabriel Anguiano <anguianog97@gmail.com>
2020-10-25 01:15:13 -0700

    socials footer section done

--- src/components/Footer/FooterContainer.js
+++ src/components/Footer/FooterContainer.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import EmailSubscribe from './EmailSubscribe/EmailSubscribe'
+import { Socials } from './Socials/Socials'
 import './Footer.scss'
 export const FooterContainer = () => {
 
     return (
         <div className='footer-container'>
             <EmailSubscribe/> 
+            <Socials/> 
         </div>
     )
 }
