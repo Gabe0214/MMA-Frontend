2a4f1f32d325dafe643f21ef1057c538e023b7e1
Gabriel Anguiano <anguianog97@gmail.com>
2020-10-26 10:04:30 -0700

    x-scroll-bar removed in New Arrivals section for a better UI look.

--- src/components/Footer/ShopFooterSection/ShopFooterSection.js
+++ src/components/Footer/ShopFooterSection/ShopFooterSection.js
@@ -6,10 +6,10 @@ export const ShopFooter = () => {
     return (
         <div className='shop-footer-container'>
             <h3 className='subtitle'>Shop</h3>
-                <ul>
-                  <li><NavLink to='/'>United States</NavLink></li>
-                  <li><NavLink to='/'>International</NavLink></li>     
-                </ul>
+            <ul>
+                <li><NavLink to='/'>United States</NavLink></li>
+                <li><NavLink to='/'>International</NavLink></li>     
+            </ul>
         </div>
     )
 }
