7b3c5de944fbbb099c4984cfaef6ea7beadc9e97
Gabriel Anguiano <anguianog97@gmail.com>
2020-10-28 16:06:19 -0700

    working on cart logic. I dont not want to append duplicate items, but I have to include price to calculate total

---
+++ src/components/Products/AddToCart/AddToCart.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react'
+
+const CartSection = () => {
+
+    return (
+        <div className='cart-section-container'>
+            
+        </div>
+    )
+}
--- src/components/Products/IndividualProduct/ProductCard.js
+++ src/components/Products/IndividualProduct/ProductCard.js
@@ -14,13 +14,14 @@ const ProductCard = ({images, name, image, setImageView, price}) => {
         <div className='product-container'>
             <img src={image} alt='AN IMAGE' className='main-img-view'/>
             <div className ='images-container'>
-                {images && images.map((item, i) => (
-                    
-                    <img src={item.img} alt='an Image' key={i} onClick={(e) =>e.preventDefault, ()=>changeMainImageView(item.img)}/>
-                ))}
+                {images && images.map((item, i) => {
+                    if(item.img !== null){
+                    return <img src={item.img} alt='an Image' key={i} onClick={(e) =>e.preventDefault, ()=>changeMainImageView(item.img)}/>
+                    }
+                })}
             </div>
             <h3 className='product-name'>{name}</h3>
-                <span className='price'>${price}</span>
+                <span className='price'>{price}</span>
         </div>
     )
 }
