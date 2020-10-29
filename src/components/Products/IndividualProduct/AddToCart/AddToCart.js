f39b5c45d2323f78e18f042c4d1eb245b6de2782
Gabriel Anguiano <anguianog97@gmail.com>
2020-10-29 14:09:05 -0700

    add to cart button implemented and styled. Once redux is implemented, functionality will commence

--- src/components/Products/IndividualProduct/AddToCart/AddToCart.js
+++ src/components/Products/IndividualProduct/AddToCart/AddToCart.js
@@ -6,7 +6,8 @@ export const CartSection = () => {
         <div className='cart-section-container'>
              <div className='quantity-container'>
                 <button className={quantity == 1 ? 'btn disabled': 'btn'} disabled={quantity == 1 ? 'disabled': null} onClick={()=>setQuanitiy(prevState => prevState - 1)}>-</button> <input className='quantity-input' value={quantity} name='quantity'/> <button onClick={()=>setQuanitiy(prevState => prevState + 1)} className='btn'>+</button> 
-             </div>  
+             </div>
+             <button className='add-to-cart'>Add To Cart</button>  
         </div>
     )
 }
