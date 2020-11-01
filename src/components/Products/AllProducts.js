7923660dc1d9dfd64f5c6033c24ca603d662efe5
Gabriel Anguiano <anguianog97@gmail.com>
2020-10-31 23:54:30 -0700

    sorting done, but in a very amateur way. gonna refactor later. filterin working on the first go. need to reset

--- src/components/Products/AllProducts.js
+++ src/components/Products/AllProducts.js
@@ -2,11 +2,12 @@ import React, { useEffect, useRef, useState } from 'react'
 import axios from 'axios'
 import { ProductsView } from './ProductsView'
 import { SortBy } from '../Filter.js/SortBy'
+import { faLiraSign } from '@fortawesome/free-solid-svg-icons'
 function AllProducts() {
 
     const [allProducts, setAllProducts] = useState([])
     const [sortOption, setSortOption] = useState('')
-
+    const [filterOption, setFilter] = useState('')
     let myRef = useRef(sortOption)
 
 
@@ -47,37 +48,66 @@ function AllProducts() {
         }))
         
     }    
-
-    const sorted = allProducts.sort((a, b) =>{
-        console.log(myRef.current)
-       if(sortOption == 'A-Z'){
-        if(a.product_name > b.product_name){
-            return 1
-        } else  return -1
-    } else if(sortOption == 'High-Low'){
-         if(Number(a.price) > Number(b.price)){
-             return -1
-         } else return 1
-    }
-    })
-
    
-    const selected = (e) => { 
-        e.preventDefault()
-        setSortOption(e.target.value)
-    }      
+    // const sorted = allProducts.sort((a, b) =>{
+    //     console.log(myRef.current)
+    //    if(sortOption == 'A-Z'){
+    //     if(a.product_name > b.product_name){
+    //         return 1
+    //     } else  return -1
+    // } else if(sortOption == 'High-Low'){
+    //      if(Number(a.price) > Number(b.price)){
+    //          return -1
+    //      } else return 1
+    // }
+    // })
+
+     function selected(e){
+         setSortOption(e.target.value)
+        sortP(e.target.value)
+    }
+    
+    function sortP(sortBy){
+             if(sortBy == 'A-Z'){
+                 allProducts.sort((a, b) => (a.product_name > b.product_name) ? 1 : -1)
+             }
+             else if(sortBy == 'Z-A'){
+                 allProducts.sort((a,b) => (a.product_name > b.product_name) ? -1 : 1)
+             }
+             else if(sortBy =='High-Low') {
+                 allProducts.sort((a, b) => (parseInt(a.price) > parseInt(b.price)) ? -1 : 1)
+             } else if(sortBy == 'Low-High'){
+                 allProducts.sort((a,b) => (parseInt(a.price) > parseInt(b.price)) ? 1 : -1)
+             }
+
+             return allProducts
+    }
+
+    function filter(filterBy){
+        
+            setAllProducts(allProducts.filter((items) => items.brand.toLowerCase().includes(filterBy.toLowerCase())))
+        
+    }
+
+    function callFilter(e){
+        setFilter(e.target.value)
+        filter(e.target.value)
+    }
+  
     return (
         <div>
             <h2 style={{textAlign:'center'}} className='title'>All Products</h2>
-            <select onChange={selected}>
+            <select onChange={selected} value={sortOption}>
                 <option value=''>Select</option>
                 <option value='A-Z'>A-Z</option>
                 <option value='Z-A'>Z-A</option>
                 <option value='High-Low'>High-Low</option>
+                <option value='Low-High'>Low-High</option>
             </select>
-            <button onClick={sortProducts}>Sort</button>
+            <button value='Affliction'onClick={callFilter}>Affliction</button>
+            <button value='Venom'onClick={callFilter}>Venom</button>
              {/* <SortBy sortIt={sortProducts} setOptions={setSortOption}/> */}
-            <ProductsView products={sorted}/>
+            <ProductsView products={allProducts}/>
         </div>
     )
 }
