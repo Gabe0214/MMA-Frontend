584d5f346de211e92b45151b4489b1946e401c4c
Gabriel Anguiano <anguianog97@gmail.com>
2020-11-06 08:31:16 -0800

    prevented duplicates in cart state. Instead, quantities of that item is updated. However, I must include the sizes, most likely. it will be an array

--- src/components/reducers/cartReducer.js
+++ src/components/reducers/cartReducer.js
@@ -10,6 +10,13 @@ const cartReducer = (state = initialState, action) => {
     
     switch(type){
         case "ADD_ITEM_TO_CART":
+            console.log(payload)
+            const duplicates = state.items.filter((product) => product.product_id == payload.product_id)
+            if(duplicates.length > 0){
+                const removeProduct = state.items.filter((product) => product.product_id !== payload.product_id)
+                const updateProductQuantity = {...duplicates[0], quantity : duplicates[0].quantity + payload.quantity}
+                return{...state, items: [...removeProduct, updateProductQuantity]}
+            }
             return {...state, items: [...state.items, payload]}
         case "CALCULATE_TOTAL":
             const addTotal = state.totalAmount + payload
