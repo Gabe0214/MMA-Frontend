9132183ebe537097003b4e728d899550c05c4144
Gabriel Anguiano <anguianog97@gmail.com>
2020-11-05 16:43:37 -0800

    almost done styling the cart page. having issues with positioning

--- src/components/ShoppingCartPage/ShoppingCart.js
+++ src/components/ShoppingCartPage/ShoppingCart.js
@@ -15,12 +15,28 @@ const ShoppingCart = () => {
     return(
         <>
         <h1 className='shopping-cart-title' >Shopping Cart</h1>
-          { shoppingCart.items.length > 0 ? <p>{shoppingCart.items.length && shoppingCart.items.length}</p>:
-           <div className='no-items-container'>
+          { shoppingCart.items.length == 0 ? <div className='no-items-container'>
              <h2 className='no-items'>There are not items in your cart</h2>
              <Link to='products/all'>Shop Here</Link>
-           </div> 
-          }
+           </div> : 
+           <div className='items-container'>
+              {shoppingCart.items.map((product, i) => (
+                <div className='item-container' key={i}>
+                  <div className='image-container'>
+                      <img src={product.img}/>
+                  </div>
+                  <div className='item-info'>
+                      <h4>{product.name}</h4>
+                      <p className='size'>Size: {product.size}</p>
+                      <p className='price'>Price: {product.price}</p>
+                  </div>
+                  <div className='remove'> 
+                      <p>x</p>
+                  </div>
+                </div>
+              ))}
+           </div>
+           }
         </>
     )
 }
