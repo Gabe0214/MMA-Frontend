0b5f11722f779cabbbf6dc82b6fd783387b3a17f
Gabriel Anguiano <anguianog97@gmail.com>
2020-11-07 10:55:09 -0800

    added remove item from nav-cart menu as well as deduct total price

--- src/components/NavBar/NavShoppingCartMenu.js/NavShoppingCartMenu.js
+++ src/components/NavBar/NavShoppingCartMenu.js/NavShoppingCartMenu.js
@@ -1,9 +1,27 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import './NavShoppingCartMenu.scss'
 
 export const NavShoppingCartMenu = () => {
     const shoppingCart  = useSelector(state => state.cart )
+    const dispatch = useDispatch()
+
+    const removeFromCart = (id, price) => {
+        dispatch({
+            type: "REMOVE_ITEM",
+            payload: {id}
+        })
+        
+        deductPrice(price)
+    }
+
+    const deductPrice = (deductPrice) => {
+        dispatch({
+            type: "DEDUCT_TOTAL",
+            payload: {total: deductPrice}
+        })
+    }
+
     return (
         <div className='menu-cart-container'>
                     {shoppingCart.items.map((product) => (
@@ -15,7 +33,7 @@ export const NavShoppingCartMenu = () => {
                             <h4>{product.name} - {product.size}</h4>
                             <span>${product.price}</span>
                         </div>
-                        <div className='remove'>
+                        <div className='remove' onClick={() =>removeFromCart(product.unique_id, product.price)}>
                             <span>x</span>
                         </div>    
                     </div>
