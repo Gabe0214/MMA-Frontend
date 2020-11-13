76b20a160dc03df89c7a6623b9385e8963d45858
Gabriel Anguiano <anguianog97@gmail.com>
2020-11-13 10:40:43 -0800

    qry products is displayed and styled. I need to add more items on the server-side. Also once that is complete, refactor redux will be implemented

--- src/components/NavBar/Menu/SubMenu/MobileSubMenu.js
+++ src/components/NavBar/Menu/SubMenu/MobileSubMenu.js
@@ -26,9 +26,9 @@ export const MobileSubMenu = ({mens, women, kids, subMenu, setSubMenu}) => {
     return (
         <div className="sub-menu-container">
            <ul className="links-container">
-        {mens ? NavLinks[0].map((link, i) => <NavLink key={i} onClick={NavClick} to={`products?gender=${link.split(' ')[0].substring(0,3).toLocaleLowerCase()}&type=${link.split(' ')[1].toLowerCase()}`}>{link}</NavLink> ): null}
-        { women ? NavLinks[1].map((link, i) => <NavLink  onClick={NavClick} key={i} to={`products?gender=${link.split(' ')[0].substring(0,5).toLowerCase()}&type=${link.split(' ')[1].toLowerCase()}`}>{link}</NavLink>): null}
-        {kids ? NavLinks[2].map((link, i) => <NavLink onClick={NavClick} key={i} to={`products?gender=${link.split(' ')[0].substring(0,3).toLowerCase()}&type=${link.split(' ')[1]}`}>{link}</NavLink>): null}
+        {mens ? NavLinks[0].map((link, i) => <NavLink key={i} onClick={NavClick} to={`/products?gender=${link.split(' ')[0].substring(0,3).toLocaleLowerCase()}&type=${link.split(' ')[1].toLowerCase()}`}>{link}</NavLink> ): null}
+        { women ? NavLinks[1].map((link, i) => <NavLink  onClick={NavClick} key={i} to={`/products?gender=${link.split(' ')[0].substring(0,5).toLowerCase()}&type=${link.split(' ')[1].toLowerCase()}`}>{link}</NavLink>): null}
+        {kids ? NavLinks[2].map((link, i) => <NavLink onClick={NavClick} key={i} to={`/products?gender=${link.split(' ')[0].substring(0,3).toLowerCase()}&type=${link.split(' ')[1]}`}>{link}</NavLink>): null}
            </ul>
         </div>
     )
--- src/components/Products/QueryProductRoutes/QuerProducts.js
+++ src/components/Products/QueryProductRoutes/QuerProducts.js
@@ -1,7 +1,12 @@
 import React, {useState, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import axios from 'axios'
+import { NavLink } from 'react-router-dom'
+import './QueryProducts.scss'
+import { ProductsView } from '../ProductsView'
 const QuerProducts = ({location}) => {
+    const [qryProducts, setQryProducts] = useState([])
+    const [isFetching, setisFecting] = useState(true)
     const arry = location.search.split('&')
     const reString = arry[0].replace('?gender=', '')
     const reStringTwo = arry[1].replace('type=', '')
@@ -15,7 +20,9 @@ const QuerProducts = ({location}) => {
         const getQueryItems = async () => {
             try {
                 const res = await axios.get(`http://localhost:8000/products${qry}`)
-                console.log(res)
+                setQryProducts(res.data)
+                setisFecting(false)
+                
             }
 
             catch (err) {
@@ -26,10 +33,29 @@ const QuerProducts = ({location}) => {
         getQueryItems()
 
     },[routed.routed])
+
+    
+
+    if(isFetching){
+        return(
+            <div style={{textAlign:'center'}}>
+                <h1>Loading...</h1>
+            </div>
+        )
+    } else if(qryProducts.length == 0){
+        return (<div className='no-qry-container'>
+                <h1>No Items Found</h1>
+                <NavLink to='/products/all'>Shop Here</NavLink>
+        </div>)
+    }
+
+    console.log(qryProducts)
     return (
-        <div>
-            <h1>{ reString !== 'unisex' ?  `${reString.toUpperCase()} ${reStringTwo.toUpperCase()}` : reStringTwo.toUpperCase()}</h1>
-        </div>
+        <section className='qry-container'>
+            <h1>{ reString !== 'unisex' ?  `${reString.toUpperCase()}'S ${reStringTwo.toUpperCase()}` : reStringTwo.toUpperCase()}</h1>
+            <div className='underline'></div>
+            <ProductsView products={qryProducts}/>
+        </section>
     )
 }
 
