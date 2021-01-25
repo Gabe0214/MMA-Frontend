957e2d49bfe17671f1dd5189919c2fe0bea78850
Gabriel Anguiano <anguianog97@gmail.com>
2021-01-25 15:14:53 -0800

    reconfigured sorting functionality due to data changed for products

--- src/components/reducers/productsReducer/productsReducer.js
+++ src/components/reducers/productsReducer/productsReducer.js
@@ -1,59 +1,63 @@
-
-
 const initialState = {
-    products: [],
-    filteredProducts: [],
-    loading: false,
-    error: false,
-    routed: false
-}
+	products: [],
+	filteredProducts: [],
+	loading: false,
+	error: false,
+	routed: false
+};
 
 const productsReducer = (state = initialState, action) => {
-    const { type, payload } = action
+	const { type, payload } = action;
 
-    switch(type){
-        case "LOAD_PRODUCTS":
-            return {...state, loading: true}
-        case "GET_ALL_PRODUCTS_SUCCESS":
-            return {...state, products: payload, filteredProducts: payload, loading: false, error:false}
-        case "GET_ALL_PRODUCTS_FAILURE":
-            return {...state, loading: false, error: true}
-        
-        case "SORT_BY_A-Z":
-            const productsState = state.products     
-            const sorted = productsState.sort((a, b) => a.product_name > b.product_name ? 1: -1).map(items => items)
-            return {...state, products: sorted }
+	switch (type) {
+		case 'LOAD_PRODUCTS':
+			return { ...state, loading: true };
+		case 'GET_ALL_PRODUCTS_SUCCESS':
+			return { ...state, products: payload, filteredProducts: payload, loading: false, error: false };
+		case 'GET_ALL_PRODUCTS_FAILURE':
+			return { ...state, loading: false, error: true };
 
-        case "SORT_BY_Z-A":
-            const productsCurrState = state.products
-            const sortedAsc= productsCurrState.sort((a, b) => a.product_name > b.product_name ? -1: 1).map(items => items)
-            return {...state, products: sortedAsc }
-        
-        case "SORT_BY_PRICE_LOW_HIGH":
-            const productsStateAgain = state.products
-            const sortedPriceLowHigh = productsStateAgain.sort((a, b) => parseInt(a.price) > parseInt(b.price) ? 1: -1).map(items => items)
-            return {...state, products: sortedPriceLowHigh }
-        
-        case "SORT_BY_PRICE_HIGH_LOW":
-            const productsStateAgainTwo = state.products
-            const sortedPriceHighLow = productsStateAgainTwo.sort((a, b) => parseInt(a.price) > parseInt(b.price) ? -1: 1).map(items => items)
-            return {...state, products: sortedPriceHighLow }
-        
-        case "FILTER_PRODUCTS":
-            const result = state.filteredProducts.filter((items ) => {
-                if(items.brand.toLowerCase().includes(payload.option.toLowerCase())){
-                    return items
-                } else if(payload.option == 'All'){ return items}
-            })
-            return {...state, products: result}
-        case "ROUTED":
-            return {...state, routed: !state.routed}
-        
-        default:
-            return state
-    }
+		case 'SORT_BY_A-Z':
+			const productsState = state.products;
+			const sorted = productsState.sort((a, b) => (a.name > b.name ? 1 : -1)).map((items) => items);
+			return { ...state, products: sorted };
 
-    return state
-}
+		case 'SORT_BY_Z-A':
+			const productsCurrState = state.products;
+			const sortedAsc = productsCurrState.sort((a, b) => (a.name > b.name ? -1 : 1)).map((items) => items);
+			return { ...state, products: sortedAsc };
 
-export default productsReducer
+		case 'SORT_BY_PRICE_LOW_HIGH':
+			const productsStateAgain = state.products;
+			const sortedPriceLowHigh = productsStateAgain
+				.sort((a, b) => (parseInt(a.price) > parseInt(b.price) ? 1 : -1))
+				.map((items) => items);
+			return { ...state, products: sortedPriceLowHigh };
+
+		case 'SORT_BY_PRICE_HIGH_LOW':
+			const productsStateAgainTwo = state.products;
+			const sortedPriceHighLow = productsStateAgainTwo
+				.sort((a, b) => (parseInt(a.price) > parseInt(b.price) ? -1 : 1))
+				.map((items) => items);
+			return { ...state, products: sortedPriceHighLow };
+
+		case 'FILTER_PRODUCTS':
+			const result = state.filteredProducts.filter((items) => {
+				if (items.brand.toLowerCase().includes(payload.option.toLowerCase())) {
+					return items;
+				} else if (payload.option == 'All') {
+					return items;
+				}
+			});
+			return { ...state, products: result };
+		case 'ROUTED':
+			return { ...state, routed: !state.routed };
+
+		default:
+			return state;
+	}
+
+	return state;
+};
+
+export default productsReducer;
