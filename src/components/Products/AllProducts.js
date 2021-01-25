5f9c52a1429209b4f70a8c10156054202ada9ba9
Gabriel Anguiano <anguianog97@gmail.com>
2021-01-25 14:55:24 -0800

    added the deployed server, and removed the local version. data is being displayed correctly.

--- src/components/Products/AllProducts.js
+++ src/components/Products/AllProducts.js
@@ -1,72 +1,74 @@
-import React, { useEffect, useRef, useState } from 'react'
-import axios from 'axios'
-import { useSelector, useDispatch } from 'react-redux'
-import { fetchAllProducts } from '../reducers/productsReducer/productsActions'
-import { ProductsView } from './ProductsView'
-import { SortBy } from '../Sort.js/SortBy'
-import { Filter } from '../Filter.js/Filter'
-import { sortProductsByDesc, sortProductsByAsc, sortProductsPriceLowHigh, sortProductsPriceHighLow, filterProducts } from '../reducers/productsReducer/productsActions'
-import './ProductsView.scss'
+import React, { useEffect, useRef, useState } from 'react';
+import axios from 'axios';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchAllProducts } from '../reducers/productsReducer/productsActions';
+import { ProductsView } from './ProductsView';
+import { SortBy } from '../Sort.js/SortBy';
+import { Filter } from '../Filter.js/Filter';
+import {
+	sortProductsByDesc,
+	sortProductsByAsc,
+	sortProductsPriceLowHigh,
+	sortProductsPriceHighLow,
+	filterProducts
+} from '../reducers/productsReducer/productsActions';
+import './ProductsView.scss';
 function AllProducts() {
+	const [ sortOption, setSortOption ] = useState('');
+	const productReducer = useSelector((state) => state.productsReducer);
+	const dispatch = useDispatch();
 
-    const [sortOption, setSortOption] = useState('')
-    const productReducer = useSelector(state => state.productsReducer)
-    const dispatch = useDispatch()
+	useEffect(() => {
+		dispatch(fetchAllProducts());
+	}, []);
 
+	useEffect(
+		() => {
+			sortP('');
+		},
+		[ sortOption ]
+	);
 
-    useEffect(() => {
-        dispatch(fetchAllProducts())
-    },[])
+	function sortP() {
+		if (sortOption == 'A-Z') {
+			dispatch(sortProductsByDesc());
+		} else if (sortOption == 'Z-A') {
+			dispatch(sortProductsByAsc());
+		} else if (sortOption == 'High-Low') {
+			dispatch(sortProductsPriceHighLow());
+		} else if (sortOption == 'Low-High') {
+			dispatch(sortProductsPriceLowHigh());
+		} else if (sortOption == '') {
+			return;
+		}
+	}
 
+	function filter(filterBy) {
+		dispatch(filterProducts(filterBy));
+	}
 
-    
-    useEffect(() => {
-        sortP('')
-    }, [sortOption])
+	if (productReducer.loading) {
+		return (
+			<React.Fragment>
+				<h1>Loading Data.....</h1>
+			</React.Fragment>
+		);
+	}
 
+	console.log(productReducer.products);
 
-    
-    function sortP(){
-             if(sortOption == 'A-Z'){
-                 dispatch(sortProductsByDesc())
-              }
-             else if(sortOption == 'Z-A'){
-                 dispatch(sortProductsByAsc())
-             }
-             else if(sortOption =='High-Low') {
-                 dispatch(sortProductsPriceHighLow())
-             } else if(sortOption == 'Low-High'){
-                 dispatch(sortProductsPriceLowHigh())
-             } else if(sortOption == ''){
-                 return 
-             }
-    }
-
-    function filter(filterBy){
-        dispatch(filterProducts(filterBy))
-     }
-
-  
-      if(productReducer.loading){
-          return (
-              <>
-              <h1>Loading Data.....</h1>
-              </>
-          )
-      }
-    
-
-
-    return (
-        <div>
-            <h2 style={{textAlign:'center'}} className='title'>All Products</h2>
-            <div className='sort-filter-container'>
-                <SortBy sortIt={sortP} setOptions={setSortOption} option={sortOption}/>
-                <Filter filterBrand={filter}/>
-            </div>
-            <ProductsView products={productReducer.products}/>
-        </div>
-    )
+	return (
+		<div>
+			<h2 style={{ textAlign: 'center' }} className='title'>
+				All Products
+			</h2>
+			<div className='sort-filter-container'>
+				<SortBy sortIt={sortP} setOptions={setSortOption} option={sortOption} />
+				<Filter filterBrand={filter} />
+			</div>
+			<ProductsView products={productReducer.products} />
+		</div>
+	);
 }
 
-export default AllProducts
+export default AllProducts;
--- src/components/reducers/productsReducer/productsActions.js
+++ src/components/reducers/productsReducer/productsActions.js
@@ -1,51 +1,48 @@
-import axios from 'axios'
+import axios from 'axios';
 
 export const loadProducts = (products) => ({
-    type: "LOAD_PRODUCTS",
-    payload: products
-})
+	type: 'LOAD_PRODUCTS',
+	payload: products
+});
 
 export const getProducts = (products) => ({
-    type: "GET_ALL_PRODUCTS_SUCCESS",
-    payload: products
-})
+	type: 'GET_ALL_PRODUCTS_SUCCESS',
+	payload: products
+});
 
 export const errorProducts = () => ({
-    type: "FETCHING_ERROR"
-})
+	type: 'FETCHING_ERROR'
+});
 
-
-export const fetchAllProducts = () => async ( dispatch ) => {
-    dispatch(loadProducts())
-    try {
-        const res = await axios.get('http://localhost:8000/products')
-        const products = res.data
-        dispatch(getProducts(products))
-    }
-
-    catch(err) {
-        console.log(err)
-        errorProducts(err)
-    }
-}
+export const fetchAllProducts = () => async (dispatch) => {
+	dispatch(loadProducts());
+	try {
+		const res = await axios.get('https://mma-server.herokuapp.com/products');
+		const products = res.data;
+		dispatch(getProducts(products));
+	} catch (err) {
+		console.log(err);
+		errorProducts(err);
+	}
+};
 
 export const sortProductsByDesc = () => ({
-    type: "SORT_BY_A-Z"
-})
+	type: 'SORT_BY_A-Z'
+});
 
 export const sortProductsByAsc = () => ({
-    type: "SORT_BY_Z-A"
-})
+	type: 'SORT_BY_Z-A'
+});
 
 export const sortProductsPriceLowHigh = () => ({
-    type: "SORT_BY_PRICE_LOW_HIGH"
-})
+	type: 'SORT_BY_PRICE_LOW_HIGH'
+});
 
 export const sortProductsPriceHighLow = () => ({
-    type: "SORT_BY_PRICE_HIGH_LOW"
-})
+	type: 'SORT_BY_PRICE_HIGH_LOW'
+});
 
 export const filterProducts = (filterOption) => ({
-    type: "FILTER_PRODUCTS",
-    payload: {option : filterOption}
-})
+	type: 'FILTER_PRODUCTS',
+	payload: { option: filterOption }
+});
