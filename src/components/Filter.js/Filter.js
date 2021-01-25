ebb9b356c0ecf82e05f540467fba680b2760ff2b
Gabriel Anguiano <anguianog97@gmail.com>
2021-01-25 15:33:34 -0800

    changed the local sever to production url in the filtered all products functionality, and it is working properly

--- src/components/Filter.js/Filter.js
+++ src/components/Filter.js/Filter.js
@@ -1,38 +1,40 @@
-import React,{useState, useEffect} from 'react'
-import axios from 'axios'
-import { FilterModal } from './FilterModal/FilterModal'
-import './Filter.scss'
-export const Filter = ({filterBrand}) => {
-    const [brands, setBrands] = useState([])
-    const [modal, setModal] = useState(false)
-    useEffect(() => {
-        const getBrands = async () => {
-            try {
-                const response =  await axios.get('http://localhost:8000/products')
-                const items= response.data.map((products) => products.brand.toLowerCase())
-                const removeDup = [...new Set(items)]
-                setBrands(removeDup.sort())
-            }
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { FilterModal } from './FilterModal/FilterModal';
+import './Filter.scss';
+export const Filter = ({ filterBrand }) => {
+	const [ brands, setBrands ] = useState([]);
+	const [ modal, setModal ] = useState(false);
+	useEffect(() => {
+		const getBrands = async () => {
+			try {
+				const response = await axios.get('https://mma-server.herokuapp.com/products');
+				const items = response.data.map((products) => products.brand.toLowerCase());
+				const removeDup = [ ...new Set(items) ];
+				setBrands(removeDup.sort());
+			} catch (err) {
+				console.log(err);
+			}
+		};
 
-            catch (err){
-                console.log(err)
-            }
-        }
+		getBrands();
+	}, []);
 
-        getBrands()
-    },[])
-
-    return (
-        <>
-            <button onClick={()=>setModal(prevState => !prevState)} className={modal ? 'filter-btn is--toggled': 'filter-btn'}><span>Filter</span> <div className='icon-filters'></div></button>
-            {/* { modal ?<ul>
+	return (
+		<React.Fragment>
+			<button
+				onClick={() => setModal((prevState) => !prevState)}
+				className={modal ? 'filter-btn is--toggled' : 'filter-btn'}
+			>
+				<span>Filter</span> <div className='icon-filters' />
+			</button>
+			{/* { modal ?<ul>
                 {brands.map((brand, i) => {
                     return <li key={i}>{brand.toUpperCase()}</li>
                 }) */}
-            
-            {/* </ul>: null } */}
-            <FilterModal modal={modal} setModal={setModal} brands={brands} filter={filterBrand}/>
 
-        </>
-    )
-}
+			{/* </ul>: null } */}
+			<FilterModal modal={modal} setModal={setModal} brands={brands} filter={filterBrand} />
+		</React.Fragment>
+	);
+};
