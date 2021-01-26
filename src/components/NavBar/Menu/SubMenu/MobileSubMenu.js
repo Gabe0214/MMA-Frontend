e887ab6cb6031727cf961d319b7b198f6cd700aa
Gabriel Anguiano <anguianog97@gmail.com>
2021-01-26 12:48:55 -0800

    had to reconfigure query components due to data change. also async call is pulling data from deployed server.

--- src/components/NavBar/Menu/SubMenu/MobileSubMenu.js
+++ src/components/NavBar/Menu/SubMenu/MobileSubMenu.js
@@ -1,38 +1,71 @@
-import React from 'react'
-import { NavLink, Link } from 'react-router-dom'
-import { items } from './subMenuLinks'
-import { useDispatch } from 'react-redux'
-import './MobileSubMenu.scss'
-export const MobileSubMenu = ({mens, women, kids, subMenu, setSubMenu}) => {
-    const dispatch = useDispatch()
-    const NavLinks = items.map((item) => {
-        return item.links
-    })
+import React from 'react';
+import { NavLink, Link } from 'react-router-dom';
+import { items } from './subMenuLinks';
+import { useDispatch } from 'react-redux';
+import './MobileSubMenu.scss';
+export const MobileSubMenu = ({ mens, women, kids, subMenu, setSubMenu }) => {
+	const dispatch = useDispatch();
+	const NavLinks = items.map((item) => {
+		return item.links;
+	});
 
-    
-   const words = "women's shirt"
-   const arrywords = words.split(' ')
-   const firstWord = arrywords[0].substring(0,5)
-   console.log(firstWord)
+	/* lines 14 - 17 is practing string configuration for qry paramaters*/
 
-   
-   const NavClick = (pathName) => {
-    console.log(pathName)
-    setSubMenu(!subMenu)
+	const words = "kid's shirt";
+	const arrywords = words.split(' ');
+	const firstWord = arrywords[0].substring(0, 6).replace("'", '');
+	console.log(firstWord);
 
-    dispatch({type:"ROUTED"})
-    
-    
+	const NavClick = (pathName) => {
+		// console.log(pathName);
+		setSubMenu(!subMenu);
 
-   }
+		dispatch({ type: 'ROUTED' });
+	};
 
-    return (
-        <div className="sub-menu-container">
-           <ul className="links-container">
-        {mens ? NavLinks[0].map((link, i) => <NavLink key={i} onClick={NavClick} to={`/products?gender=${link.split(' ')[0].substring(0,3).toLocaleLowerCase()}&type=${link.split(' ')[1].toLowerCase()}`}>{link}</NavLink> ): null}
-        { women ? NavLinks[1].map((link, i) => <NavLink  onClick={NavClick} key={i} to={`/products?gender=${link.split(' ')[0].substring(0,5).toLowerCase()}&type=${link.split(' ')[1].toLowerCase()}`}>{link}</NavLink>): null}
-        {kids ? NavLinks[2].map((link, i) => <NavLink onClick={NavClick} key={i} to={`/products?gender=${link.split(' ')[0].substring(0,3).toLowerCase()}&type=${link.split(' ')[1].toLowerCase()}`}>{link}</NavLink>): null}
-           </ul>
-        </div>
-    )
-}
+	return (
+		<div className='sub-menu-container'>
+			<ul className='links-container'>
+				{mens ? (
+					NavLinks[0].map((link, i) => (
+						<NavLink
+							key={i}
+							onClick={NavClick}
+							to={`/products?product_for=${link.split(' ')[0].substring(0, 3).toLocaleLowerCase()}&type=${link
+								.split(' ')[1]
+								.toLowerCase()}`}
+						>
+							{link}
+						</NavLink>
+					))
+				) : null}
+				{women ? (
+					NavLinks[1].map((link, i) => (
+						<NavLink
+							onClick={NavClick}
+							key={i}
+							to={`/products?product_for=${link.split(' ')[0].substring(0, 5).toLowerCase()}&type=${link
+								.split(' ')[1]
+								.toLowerCase()}`}
+						>
+							{link}
+						</NavLink>
+					))
+				) : null}
+				{kids ? (
+					NavLinks[2].map((link, i) => (
+						<NavLink
+							onClick={NavClick}
+							key={i}
+							to={`/products?product_for=${link.split(' ')[0].substring(0, 3).toLowerCase()}&type=${link
+								.split(' ')[1]
+								.toLowerCase()}`}
+						>
+							{link}
+						</NavLink>
+					))
+				) : null}
+			</ul>
+		</div>
+	);
+};
