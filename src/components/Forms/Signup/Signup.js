ff6517ac0f814bb8e391467aa531c84ffebf92cf
Gabriel Anguiano <anguianog97@gmail.com>
2021-01-27 16:38:26 -0800

    added form validation for signup component.

--- src/components/Forms/Signup/Signup.js
+++ src/components/Forms/Signup/Signup.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { useForm } from '../useForm/useForm';
-
+import { states } from './fiftyStates';
 const SignupForm = () => {
 	const onSubmit = (e) => {
 		e.preventDefault();
 		console.log(values);
+		resetFields();
 	};
-	const [ values, handleChanges ] = useForm();
+	const [ values, handleChanges, resetFields, errors ] = useForm();
 
 	return (
 		<div>
@@ -23,6 +24,37 @@ const SignupForm = () => {
 				<label>Last Name</label>
 				<div>
 					<input name='lastname' value={values.lastname || ''} onChange={handleChanges} />
+				</div>
+				<label>Username</label>
+				<div>
+					<input name='username' value={values.username || ''} onChange={handleChanges} />
+				</div>
+				<label>Email</label>
+				<div>
+					<input name='email' value={values.email || ''} onChange={handleChanges} />
+				</div>
+				<label>Password</label>
+				<div>
+					<input name='password' type='password' value={values.password || ''} onChange={handleChanges} />
+				</div>
+				<label>Zip Code</label>
+				<div>
+					<input name='zip' value={values.zip || ''} onChange={handleChanges} />
+				</div>
+				<label>City</label>
+				<div>
+					<input name='city' value={values.city || ''} onChange={handleChanges} />
+				</div>
+				<label>State</label>
+				<div>
+					<select value={values.state || ''} name='state' onChange={handleChanges}>
+						<option>Select State</option>
+						{states.map((state) => <option value={state}>{state}</option>)}
+					</select>
+				</div>
+				<label>Address</label>
+				<div>
+					<input name='adress' value={values.adress || ''} onChange={handleChanges} />
 				</div>
 				<button>Sign Up</button>
 			</form>
--- src/components/Forms/useForm/useForm.js
+++ src/components/Forms/useForm/useForm.js
@@ -3,9 +3,12 @@ import { useState } from 'react';
 export const useForm = () => {
 	const [ state, setState ] = useState({});
 
+	const resetFields = () => {
+		setState({});
+	};
 	const handleChanges = (e) => {
 		setState({ ...state, [e.target.name]: e.target.value });
 	};
 
-	return [ state, handleChanges ];
+	return [ state, handleChanges, resetFields ];
 };
