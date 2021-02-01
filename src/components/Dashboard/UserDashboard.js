3777f0cbfa3c8385886332cd13c37b308010704b
Gabriel Anguiano <anguianog97@gmail.com>
2021-02-01 09:55:26 -0800

    account details pretty much complete. will add an edit button, which will let user edit their infoormation.

--- src/components/Dashboard/UserDashboard.js
+++ src/components/Dashboard/UserDashboard.js
@@ -1,15 +1,41 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
+import './dashboard.scss';
+
 const UserDashboard = () => {
 	const history = useHistory();
 	const logout = () => {
 		localStorage.clear();
 		history.push('/signin');
 	};
+
+	const customer = useSelector((state) => state.customer);
+
 	return (
-		<div>
-			<h1>This must be secured</h1>
-			<button onClick={logout}>Logout</button>
+		<div className='dashboard-container'>
+			<div className='title-container'>
+				<h1>Account Details</h1>
+				<div className='underline' />
+			</div>
+			<div className='user-content'>
+				<div className='sub-content'>
+					<h5>{`${customer.user.firstname[0].toUpperCase() +
+						customer.user.firstname.slice(1)} ${customer.user.lastname[0].toUpperCase() +
+						customer.user.lastname.slice(1)}`}</h5>
+
+					<p>{customer.user.email}</p>
+				</div>
+				<div className='sub-content'>
+					<h5>Primary Address</h5>
+					<p>{customer.user.adress}</p>
+					<p>{`${customer.user.city}, ${customer.user.state}, ${customer.user.zip}`}</p>
+					<p>United States</p>
+				</div>
+			</div>
+			<button onClick={logout} className='form-btn'>
+				Logout
+			</button>
 		</div>
 	);
 };
