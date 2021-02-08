77b524af47bc385277b00cc133f45f34ef4e07e0
Gabriel Anguiano <anguianog97@gmail.com>
2021-02-08 11:08:23 -0800

    footer responsiveness completee for 800px screen width

--- src/components/Footer/FooterContainer.js
+++ src/components/Footer/FooterContainer.js
@@ -1,22 +1,25 @@
-import React from 'react'
-import EmailSubscribe from './EmailSubscribe/EmailSubscribe'
-import { Socials } from './Socials/Socials'
-import { AboutCompany } from './AboutCompany/AboutCompany'
-import { ShopFooter } from './ShopFooterSection/ShopFooterSection'
-import './Footer.scss'
+import React from 'react';
+import EmailSubscribe from './EmailSubscribe/EmailSubscribe';
+import { Socials } from './Socials/Socials';
+import { AboutCompany } from './AboutCompany/AboutCompany';
+import { ShopFooter } from './ShopFooterSection/ShopFooterSection';
+import './Footer.scss';
 export const FooterContainer = () => {
-
-    return (
-     <>
-        <div className='footer-container'>
-            <EmailSubscribe/> 
-            <Socials/> 
-            <AboutCompany/>
-            <ShopFooter/>  
-        </div>
-        <div className='copy-right'>
-            <h5><span>&#169;</span> 2020 Gabriel Anguiano</h5>
-        </div>
-     </>
-    )
-}
+	return (
+		<React.Fragment>
+			<section id='footer'>
+				<div className='footer-inner-container'>
+					<EmailSubscribe />
+					<Socials />
+					<AboutCompany />
+					<ShopFooter />
+				</div>
+			</section>
+			<div className='copy-right'>
+				<h5>
+					<span>&#169;</span> 2020 Gabriel Anguiano
+				</h5>
+			</div>
+		</React.Fragment>
+	);
+};
--- src/components/Footer/Socials/Socials.js
+++ src/components/Footer/Socials/Socials.js
@@ -1,31 +1,31 @@
-import React from 'react'
-import { faFacebookSquare } from '@fortawesome/free-brands-svg-icons'
-import { faTwitterSquare } from '@fortawesome/free-brands-svg-icons'
-import { faInstagramSquare } from '@fortawesome/free-brands-svg-icons'
-import { faYoutubeSquare } from '@fortawesome/free-brands-svg-icons'
-import { faEnvelopeSquare } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import './Socials.scss'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { faFacebookSquare } from '@fortawesome/free-brands-svg-icons';
+import { faTwitterSquare } from '@fortawesome/free-brands-svg-icons';
+import { faInstagramSquare } from '@fortawesome/free-brands-svg-icons';
+import { faYoutubeSquare } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelopeSquare } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import './Socials.scss';
+import { Link } from 'react-router-dom';
 export const Socials = () => {
+	const socialIcons = [
+		{ name: faFacebookSquare, url: 'facebook.com', class: 'social' },
+		{ name: faTwitterSquare, url: 'twitter.com', class: 'social' },
+		{ name: faInstagramSquare, url: 'instagram.com', class: 'social' },
+		{ name: faYoutubeSquare, url: 'youtube.com', class: 'social' },
+		{ name: faEnvelopeSquare, url: 'GabeWebDev097@gmail.com', class: 'social' }
+	];
 
-    const socialIcons = [
-        {name: faFacebookSquare, url: 'facebook.com', class: 'social'}, 
-        {name: faTwitterSquare, url: 'twitter.com', class: 'social'}, 
-        {name: faInstagramSquare, url: 'instagram.com', class: 'social'}, 
-        {name: faYoutubeSquare, url: 'youtube.com', class: 'social'}, 
-        {name: faEnvelopeSquare, url: 'GabeWebDev097@gmail.com', class: 'social'}
-    ]
-
-    return (
-        <div className='socials-container'>
-            <h3 className='subtitle'>Socials</h3>
-            {socialIcons.map((icon, i) => (
-                <a key ={i} href={!icon.url.includes('@') ?`https://${icon.url}`:`mailto:${icon.url}`} target='_blank'>
-                    <FontAwesomeIcon icon={icon.name} className={icon.class} size='lg' key={i}/> 
-                </a>
-            ))}
-
-        </div>
-    )
-}
+	return (
+		<div className='socials-container'>
+			<h3 className='subtitle'>Socials</h3>
+			<div className='socials-icon-container'>
+				{socialIcons.map((icon, i) => (
+					<a key={i} href={!icon.url.includes('@') ? `https://${icon.url}` : `mailto:${icon.url}`} target='_blank'>
+						<FontAwesomeIcon icon={icon.name} className={icon.class} size='lg' key={i} />
+					</a>
+				))}
+			</div>
+		</div>
+	);
+};
