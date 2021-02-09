e5d863a31253a3e317347046b73a9ceda8a7db7f
Gabriel Anguiano <anguianog97@gmail.com>
2021-02-09 12:12:03 -0800

    responsiveness for cart menu complete. found a bug when user removes item in nav bar cart menu. it has been fixed

--- src/components/NavBar/NavShoppingCartMenu.js/NavShoppingCartMenu.js
+++ src/components/NavBar/NavShoppingCartMenu.js/NavShoppingCartMenu.js
@@ -1,56 +1,64 @@
-import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faLock } from '@fortawesome/free-solid-svg-icons'
-import { useHistory } from 'react-router-dom'
-import './NavShoppingCartMenu.scss'
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLock } from '@fortawesome/free-solid-svg-icons';
+import { useHistory } from 'react-router-dom';
+import './NavShoppingCartMenu.scss';
 
 export const NavShoppingCartMenu = () => {
-    const shoppingCart  = useSelector(state => state.cart )
-    const dispatch = useDispatch()
-    const history = useHistory()
-    const removeFromCart = (id, price) => {
-        dispatch({
-            type: "REMOVE_ITEM",
-            payload: {id}
-        })
-        
-        deductPrice(price)
-    }
+	const shoppingCart = useSelector((state) => state.cart);
+	const dispatch = useDispatch();
+	const history = useHistory();
+	const removeFromCart = (id, price, qty) => {
+		const totalItemAmount = price * qty;
+		dispatch({
+			type: 'REMOVE_ITEM',
+			payload: { id }
+		});
 
-    const deductPrice = (deductPrice) => {
-        dispatch({
-            type: "DEDUCT_TOTAL",
-            payload: {total: deductPrice}
-        })
-    }
+		deductPrice(totalItemAmount);
+	};
 
-    return (
-        <div className={ shoppingCart.showNavMenu ? 'menu-cart-container menu menu--active' : 'menu-cart-container'}>
-                    <h4 style={{textAlign:'center', fontSize:'12px', padding:'15px 0'}}>Shopping Cart ({shoppingCart.items.length})</h4>
-                    {shoppingCart.items.map((product) => (
-                    <div className='item-container'>
-                        <div className='image-container'>
-                            <img src={product.img} alt='proudt-image'/>
-                        </div>
-                        <div className='item-info'>
-                            <h4>{product.name} - {product.size}</h4>
-                            <span>${product.price}</span>
-                        </div>
-                        <div className='remove' onClick={() =>removeFromCart(product.unique_id, product.price)}>
-                            <span>x</span>
-                        </div>    
-                    </div>
-                    ))}
-                    <div className='go-to-cart-container'>
-                        <div className='sub-total-container'>
-                            <span>Subtotal:</span>
-                            <span className='money'>$ {shoppingCart.totalAmount} USD</span>
-                        </div>
-                        <div className='go-to-cart-btn'>
-                            <button onClick={()=>history.push('/cart')}><FontAwesomeIcon icon={faLock}/> Go to cart</button>
-                        </div>
-                    </div>
-        </div>
-    )
-}
+	const deductPrice = (deductPrice) => {
+		dispatch({
+			type: 'DEDUCT_TOTAL',
+			payload: { total: deductPrice }
+		});
+	};
+	// console.log(shoppingCart.items);
+
+	return (
+		<div className={shoppingCart.showNavMenu ? 'menu-cart-container menu menu--active' : 'menu-cart-container'}>
+			<h4 style={{ textAlign: 'center', fontSize: '12px', padding: '15px 0' }}>
+				Shopping Cart ({shoppingCart.items.length})
+			</h4>
+			{shoppingCart.items.map((product) => (
+				<div className='item-container'>
+					<div className='image-container'>
+						<img src={product.img} alt='proudt-image' />
+					</div>
+					<div className='item-info'>
+						<h4>
+							{product.name} - {product.size}
+						</h4>
+						<span>${product.price}</span>
+					</div>
+					<div className='remove' onClick={() => removeFromCart(product.unique_id, product.price, product.quantity)}>
+						<span>x</span>
+					</div>
+				</div>
+			))}
+			<div className='go-to-cart-container'>
+				<div className='sub-total-container'>
+					<span>Subtotal:</span>
+					<span className='money'>$ {shoppingCart.totalAmount} USD</span>
+				</div>
+				<div className='go-to-cart-btn'>
+					<button onClick={() => history.push('/cart')}>
+						<FontAwesomeIcon icon={faLock} /> Go to cart
+					</button>
+				</div>
+			</div>
+		</div>
+	);
+};
