ce417a3984fc686b1e84e0969a1ebe953e77e804
Gabriel Anguiano <anguianog97@gmail.com>
2021-02-09 09:58:18 -0800

    home page and forms responsiveness complete for 1366 screen width

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -13,7 +13,8 @@ const HomePage = ({ cart, setCart }) => {
 		infinite: true,
 		speed: 500,
 		slidesToShow: 1,
-		slidesToScroll: 1
+		slidesToScroll: 1,
+		autoplay: true
 	};
 
 	const addItem = (e) => {
