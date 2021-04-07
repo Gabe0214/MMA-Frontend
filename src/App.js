d87ab990e4d1c6f3b3aa275f039e4d8b36e0fb9f
Gabriel Anguiano <anguianog97@gmail.com>
2021-04-07 14:36:31 -0700

    cleaned up app component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import HomePage from './components/Home/Home';
 import ShoppingCart from './components/ShoppingCartPage/ShoppingCart';
