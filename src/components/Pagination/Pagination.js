68b4338860de252f097bef64561a9836692b8ee2
Gabriel Anguiano <anguianog97@gmail.com>
2021-04-15 17:44:51 -0700

    pagination logic almost done

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -11,34 +11,36 @@ export const Pagination = ({ totalPosts, postsPerPage, paginate, setCurrentPage 
 	}
 
 	const onPageClick = (page) => {
-		if (pageNumber[pageNumber.length - 1 - 4] === page) {
-			console.log(pageNumber.slice(page, pageNumber[pageNumber.length - 2]));
-			setSliceStart(page);
-			setSliceEnd(pageNumber[pageNumber.length - 2]);
-			return paginate(page);
-		} else if (pageNumber[pageNumber.length - 1 - 4] < page) return paginate(page);
-		paginate(page);
-		setSliceStart((prevState) => prevState + 1);
-		setSliceEnd((preveState) => preveState + 1);
+		// if (pageNumber[pageNumber.length - 1 - 4] === page) {
+		// 	console.log(pageNumber.slice(page, pageNumber[pageNumber.length - 2]));
+		// 	setSliceStart(page);
+		// 	setSliceEnd(pageNumber[pageNumber.length - 2]);
+		// 	return paginate(page);
+		// } else if (pageNumber[pageNumber.length - 1 - 4] < page) return paginate(page);
+		console.log(pagesStart);
+		if (Math.ceil(pageNumber[pageNumber.length - 1] / 2 - 1) === page) {
+			setSliceStart(page - 2);
+			setSliceEnd(page + 1);
+			paginate(page);
+		} else {
+			paginate(page);
+		}
+		// setSliceStart((prevState) => prevState + 1);
+		// setSliceEnd((preveState) => preveState + 1);
 	};
 
 	const backOnePage = () => {
-		// if (sliceStart <= 1) {
-		// 	return;
-		// } else {
-		// 	setSliceStart((prevState) => prevState - 1);
-		// }
 		setCurrentPage((currentPage) => (currentPage <= 1 ? 1 : currentPage - 1));
 	};
 
 	const pagesStart = pageNumber.slice(sliceStart, sliceEnd);
-	console.log({ sliceStart, sliceEnd });
+
 	return (
 		<div className='pages-container'>
 			<ul>
 				<li onClick={backOnePage}>B</li>
 				<li>{pageNumber[0]}</li>
-				{sliceStart >= 7 ? <li>...</li> : ''}
+				{sliceStart >= 3 ? <li>...</li> : ''}
 				{pagesStart.map((page) => {
 					return (
 						<li key={page} onClick={() => onPageClick(page)}>
